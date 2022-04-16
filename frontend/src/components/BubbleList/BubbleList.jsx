b9b6a737efd08bc539eebbb256152b2f5fa4f825
Joel Braun <joel22b@gmail.com>
2022-04-16 13:26:18 -0400

    Automated colours for skills and updated the skills

--- frontend/src/components/BubbleList/BubbleList.jsx
+++ frontend/src/components/BubbleList/BubbleList.jsx
@@ -1,10 +1,12 @@
 import './BubbleList.css';
 
+import colours from '../../generic/data/skillsColours.json'
+
 function BubbleList({skills}) {
     return (
         <div className="flex-container">
             {
-                skills.map((skillObject, i) => <div className="skill" style={{"color": skillObject.colour}} key={i}>{skillObject.skill}</div>)
+                skills.map((skill, i) => <div className="skill" style={{"color": colours.colours[i % colours.length]}} key={i}>{skill}</div>)
             }
         </div>
     );
